4394fccc705e19b9aca8ec9c9079f36818b551e0
仁志 <legend1world@gmail.com>
2023-04-30 11:54:52 +0900

    up

--- csvtojson.js
+++ csvtojson.js
@@ -8,7 +8,15 @@ const result = await parse(data);
 await Deno.writeTextFile("data/all.json", JSON.stringify(result));
 
 
-let date = new Date();
+let dt = new Date();
+
+let ts = dt.getTime();
+
+let ts_after = ts + (1000 * 60 * 60 * 9);
+
+let date = new Date(ts_after);
+
+console.log(date);
 
 const year = date.getFullYear();
 
@@ -20,8 +28,6 @@ const hour = date.getHours();
 
 const minute = date.getMinutes();
 
-
 const now_day = year + "年" + month + "月" + day + "日" + hour + "時" + minute + "分";
-
 
 const write = Deno.writeTextFile("./data/lastupdate.json", '[{"lastupdate":' + '"' + now_day + '"}]');
